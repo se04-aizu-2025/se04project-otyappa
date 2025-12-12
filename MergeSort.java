aae15da73578c2098244561a97fb9ed6e75fa0e3
s1310036 <s1310036@u-aizu.ac.jp>
2025-12-12 16:26:46 +0900

    Fix merge function variable initialization

--- MergeSort.java
+++ MergeSort.java
@@ -1,4 +1,11 @@
+package sorting;
+
 public class MergeSort implements Sorter {
+
+    @Override
+    public String name() {
+        return "Merge Sort";
+    }
 
     @Override
     public void sort(int[] arr) {
@@ -7,15 +14,19 @@ public class MergeSort implements Sorter {
 
     private void mergeSortRecursive(int[] arr, int left, int right) {
         if (left >= right) return;
+
         int mid = (left + right) / 2;
 
         mergeSortRecursive(arr, left, mid);
         mergeSortRecursive(arr, mid + 1, right);
+
         merge(arr, left, mid, right);
     }
 
     private void merge(int[] arr, int left, int mid, int right) {
-        int n1 = mid - left + 1, n2 = right - mid;
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
         int[] L = new int[n1];
         int[] R = new int[n2];
 
@@ -23,10 +34,12 @@ public class MergeSort implements Sorter {
         for (int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
 
         int i = 0, j = 0, k = left;
+
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) arr[k++] = L[i++];
             else arr[k++] = R[j++];
         }
+
         while (i < n1) arr[k++] = L[i++];
         while (j < n2) arr[k++] = R[j++];
     }
